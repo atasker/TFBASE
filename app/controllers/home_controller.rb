fd05cf17cdda9c704e23588f707631aa74d35edf
Ivan Dymkov <mstrdymio@gmail.com>
2017-08-05 18:14:41 +0700

    Finish homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,14 +12,14 @@ class HomeController < BaseFrontendController
         place: 'London, UK',
         bg_img_url: '/content/main1.jpg',
         start_date: Date.new(2017, 1, 12),
-        end_date: Date.new(2017, 2, 14),
+        end_date: Date.new(2017, 2, 4),
         event_url: '#'
       ),
       OpenStruct.new(
         title: 'Test 1',
         place: 'test, OK',
         bg_img_url: '/content/main1.jpg',
-        start_date: Date.new(2017, 2, 14),
+        start_date: Date.new(2017, 2, 4),
         end_date: nil,
         event_url: '#'
       ),
@@ -40,6 +40,172 @@ class HomeController < BaseFrontendController
              each do |spec_category|
       @main_categories[spec_category.description] = spec_category.id
     end
+
+    # Dummy of feature slides
+    @featured_slides = [
+      OpenStruct.new(
+        title: 'Kentucky Derby Tickets',
+        place: 'Barcelona, Spain',
+        start_date: Date.new(2017, 12, 14),
+        end_date: nil,
+        bg_img_url: '/content/f-test1.jpg',
+        url: '#',
+        labels: [
+          OpenStruct.new(name: 'Horse Racing', url: '#', sqheme_color: 'green'),
+          OpenStruct.new(name: 'Special Events', url: '#', sqheme_color: 'yellow')
+        ]
+      ),
+      OpenStruct.new(
+        title: 'Champions League 2016/17 Tickets',
+        place: 'Leverkusen, Germany',
+        start_date: Date.new(2017, 12, 14),
+        end_date: Date.new(2018, 2, 28),
+        bg_img_url: '/content/f-test2.jpg',
+        url: '#',
+        labels: [
+          OpenStruct.new(name: 'Football', url: '#', sqheme_color: 'green')
+        ]
+      ),
+      OpenStruct.new(
+        title: "London's West End Musical Theatre Tickets",
+        place: 'Moscow, Russia',
+        start_date: Date.new(2018, 2, 28),
+        end_date: nil,
+        bg_img_url: '/content/f-test3.jpg',
+        url: '#',
+        labels: [
+          OpenStruct.new(name: 'Horse Racing', url: '#', sqheme_color: 'green'),
+          OpenStruct.new(name: 'Special Events', url: '#', sqheme_color: 'yellow')
+        ]
+      ),
+      OpenStruct.new(
+        title: 'Formula 1 Tickets',
+        place: 'Barcelona, Spain',
+        start_date: nil,
+        end_date: nil,
+        bg_img_url: '/content/f-test4.jpg',
+        url: '#',
+        labels: [
+          OpenStruct.new(name: 'Motor Racing', url: '#', sqheme_color: 'green')
+        ]
+      ),
+      OpenStruct.new(
+        title: 'Kentucky Derby Tickets',
+        place: 'Barcelona, Spain',
+        start_date: Date.new(2017, 12, 14),
+        end_date: Date.new(2018, 2, 28),
+        bg_img_url: '/content/f-test2.jpg',
+        url: '#',
+        labels: [
+          OpenStruct.new(name: 'Horse Racing', url: '#', sqheme_color: 'green'),
+          OpenStruct.new(name: 'Special Events', url: '#', sqheme_color: 'yellow')
+        ]
+      )
+    ]
+
+    # Dummy of "top events"
+    @top_events = [
+      OpenStruct.new(
+        title: 'Champions League 2016/17 Tickets',
+        place: 'Yas Marina, Abu Dhabi',
+        start_date: Date.new(2017, 12, 14),
+        end_date: Date.new(2018, 2, 28),
+        bg_img_url: '/content/m-test1.jpg',
+        url: '#',
+        label: 'Test'
+      ),
+      OpenStruct.new(
+        title: 'Champions League 2016/17 Tickets',
+        place: 'Yas Marina, Abu Dhabi',
+        start_date: Date.new(2017, 12, 14),
+        end_date: nil,
+        bg_img_url: '/content/m-test2.jpg',
+        url: '#',
+        label: 'Motor racing'
+      ),
+      OpenStruct.new(
+        title: 'Champions League 2016/17 Tickets',
+        place: 'Yas Marina, Abu Dhabi',
+        start_date: Date.new(2018, 2, 28),
+        end_date: nil,
+        bg_img_url: '/content/m-test3.jpg',
+        url: '#',
+        label: 'Motor racing'
+      ),
+      OpenStruct.new(
+        title: 'Champions League 2016/17 Tickets',
+        place: 'Yas Marina, Abu Dhabi',
+        start_date: Date.new(2017, 12, 14),
+        end_date: Date.new(2018, 2, 28),
+        bg_img_url: '/content/m-test4.jpg',
+        url: '#',
+        label: 'test'
+      )
+    ]
+
+    # Dummy of "popular events"
+    @popular_events = [
+      OpenStruct.new(
+        title: 'Champions League 2016/17 Tickets',
+        place: 'Barcelona, Spain',
+        start_date: Date.new(2017, 12, 14),
+        end_date: Date.new(2018, 2, 28),
+        bg_img_url: '/content/p-test1.jpg',
+        url: '#',
+        labels: [
+          OpenStruct.new(name: 'Golf', url: '#', sqheme_color: 'green'),
+          OpenStruct.new(name: 'Special Events', url: '#', sqheme_color: 'yellow')
+        ]
+      ),
+      OpenStruct.new(
+        title: 'The British Open 2017 Tickets',
+        place: 'Barcelona, Spain',
+        start_date: Date.new(2018, 2, 28),
+        end_date: nil,
+        bg_img_url: '/content/p-test2.jpg',
+        url: '#',
+        labels: [
+          OpenStruct.new(name: 'Motorcycling', url: '#', sqheme_color: 'green')
+        ]
+      ),
+      OpenStruct.new(
+        title: 'Champions League 2016/17 Tickets',
+        place: 'Barcelona, Spain',
+        start_date: Date.new(2018, 2, 28),
+        end_date: nil,
+        bg_img_url: '/content/p-test1.jpg',
+        url: '#',
+        labels: [
+          OpenStruct.new(name: 'Golf', url: '#', sqheme_color: 'green'),
+          OpenStruct.new(name: 'Special Events', url: '#', sqheme_color: 'yellow')
+        ]
+      ),
+      OpenStruct.new(
+        title: 'The British Open 2017 Tickets',
+        place: 'Barcelona, Spain',
+        start_date: Date.new(2017, 12, 14),
+        end_date: Date.new(2018, 2, 28),
+        bg_img_url: '/content/p-test2.jpg',
+        url: '#',
+        labels: [
+          OpenStruct.new(name: 'Motorcycling', url: '#', sqheme_color: 'green')
+        ]
+      )
+    ]
+
+    # Dummy of "main services"
+    @main_services = [
+      OpenStruct.new(name: 'Arsenal', url: '#', bg_img_url: '/content/item1.jpg'),
+      OpenStruct.new(name: 'Barselona', url: '#', bg_img_url: '/content/item2.jpg'),
+      OpenStruct.new(name: 'Wimbledone', url: '#', bg_img_url: '/content/item3.jpg'),
+      OpenStruct.new(name: 'Tottenham', url: '#', bg_img_url: '/content/item4.jpg'),
+      OpenStruct.new(name: 'French open', url: '#', bg_img_url: '/content/item5.jpg'),
+      OpenStruct.new(name: 'Valencia', url: '#', bg_img_url: '/content/item6.jpg'),
+      OpenStruct.new(name: 'New Year 2017', url: '#', bg_img_url: '/content/item7.jpg'),
+      OpenStruct.new(name: 'Barselona', url: '#', bg_img_url: '/content/item8.jpg'),
+      OpenStruct.new(name: 'Test', url: '#', bg_img_url: '/content/item9.jpg'),
+      OpenStruct.new(name: 'Test', url: '#', bg_img_url: '/content/item5.jpg')
+    ]
 
     # @sports = Event.where(sports: true).sample(3)
     # @concerts_id = Category.where(description: "Concerts").ids
