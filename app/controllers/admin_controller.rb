000601785e27ac93b075bce7c720dece237a3074
Ivan Dymkov <mstrdymio@gmail.com>
2017-08-11 13:11:41 +0700

    Remove auth credentials from a controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,7 @@ class AdminController < ApplicationController
 
   layout "admin"
 
-  http_basic_authenticate_with name: "adam", password: "password"
+  http_basic_authenticate_with name: ENV.fetch('ADMIN_NAME', 'admin'),
+                               password: ENV.fetch('ADMIN_PASSWORD', 'password')
 
 end
