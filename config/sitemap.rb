47c79aa97c5b1bcae43cadc89f4771b554e336df
Ivan Dymkov <mstrdymio@gmail.com>
2017-09-20 13:50:50 +0700

    Update protocol in url's of the site

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://www.ticket-finders.com"
+SitemapGenerator::Sitemap.default_host = "https://www.ticket-finders.com"
 
 SitemapGenerator::Sitemap.create do
 
