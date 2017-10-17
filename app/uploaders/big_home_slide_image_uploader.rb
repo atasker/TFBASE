713da4bc361081b02912666fc190abfd3655ceec
Ivan Dymkov <mstrdymio@gmail.com>
2017-10-17 19:25:53 +0700

    Add image optimization for big home slide image uploader

--- app/uploaders/big_home_slide_image_uploader.rb
+++ app/uploaders/big_home_slide_image_uploader.rb
@@ -1,9 +1,11 @@
 class BigHomeSlideImageUploader < BaseImageUploader
   version :the_first do
     process resize_to_fill: [686, 686]
+    process :optimize_for_pagespeed
   end
 
   version :other do
     process resize_to_fill: [686, 300]
+    process :optimize_for_pagespeed
   end
 end
