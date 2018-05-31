ac55f9e66190f41b4ae59a2169416d0962716c85
Ivan Dymkov <mstrdymio@gmail.com>
2018-06-01 00:27:23 +0700

    Temporary disable admin password and metrics for staging version

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,7 +2,7 @@ class AdminController < ApplicationController
 
   layout "admin"
 
-  http_basic_authenticate_with name: ENV.fetch('ADMIN_NAME', 'admin'),
-                               password: ENV.fetch('ADMIN_PASSWORD', 'password')
+  # TODO uncomment for production ENDTODO http_basic_authenticate_with name: ENV.fetch('ADMIN_NAME', 'admin'),
+  # TODO uncomment for production ENDTODO                              password: ENV.fetch('ADMIN_PASSWORD', 'password')
 
 end
