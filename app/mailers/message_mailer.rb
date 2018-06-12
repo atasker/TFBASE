2815e74bc87b7a91190268d219ca43886fb3e9e0
Ivan Dymkov <mstrdymio@gmail.com>
2018-06-12 18:12:21 +0700

    Prepare email test

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -2,20 +2,24 @@ class MessageMailer < ApplicationMailer
 
   def send_email(message)
     @message = message
-    recipients = ["john@ticket-finders.com", "info@ticket-finders.com"]
     mail(:to => recipients, :subject => "New contact form submission at ticket-finders.com")
   end
 
   def send_hospitality_concierge_email(client_email)
     @client_email = client_email
-    recipients = ["john@ticket-finders.com", "info@ticket-finders.com"]
     mail(:to => recipients, :subject => "New Hospitality & Concierge services request at ticket-finders.com")
   end
 
   def send_ticket_enquiry(enquiry)
     @enquiry = enquiry
-    recipients = ["john@ticket-finders.com", "info@ticket-finders.com"]
     mail(:to => recipients, :subject => "New ticket enquiry at ticket-finders.com")
+  end
+
+  private
+
+  def recipients
+    # ["john@ticket-finders.com", "info@ticket-finders.com"]
+    ['mstrdymio@gmail.com']
   end
 
 end
