c1d6d1c5a7889f6b91d8cc3cd28ac23bfe7e2feb
Ivan Dymkov <mstrdymio@gmail.com>
2018-06-21 02:54:13 +0700

    Add design of account home page

--- app/controllers/users/profile_controller.rb
+++ app/controllers/users/profile_controller.rb
@@ -6,7 +6,8 @@ class Users::ProfileController < BaseFrontendController
   before_action :add_users_breadcrumb
 
   def show
-
+    @orders = current_user.orders.includes(:items).order(created_at: :desc).limit(6)
+    @have_more_orders = current_user.orders.count > 6
   end
 
   private
